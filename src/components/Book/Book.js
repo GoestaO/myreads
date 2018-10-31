250b8c1be3e0d244d441e0103cebc0d31f41ef1e
Gösta Ostendorf <goesta.o@gmail.com>
2018-10-31 07:30:17 +0100

    Revert "Master"

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import BookshelfChanger from '../BookshelfChanger/BookshelfChanger';
-import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const {book, setNewShelf} = props;
-
-  // If there's no imageLink, hide this style property
-  const style = (book.imageLinks)
-    ? ({width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}`})
-    : ({width: 128, height: 193});
+  const {book} = props;  
   return (<div className="book">
     <div className="book-top">
-      <div className="book-cover" style={style}></div>
+      <div className="book-cover" style={{
+          width: 128,
+          height: 193,
+          backgroundImage: `url(${book.imageLinks.thumbnail}`
+        }}></div>
       <div>
-        <BookshelfChanger book={book} setNewShelf={setNewShelf}/>
+        <BookshelfChanger/>
       </div>
     </div>
     <div className="book-title">{book.title}</div>
-    <div className="book-authors">
-      {/* Render all authors, in case of no existing author render nothing */}
-      {
-        (book.authors)
-          ? (book.authors.map((author, index) => <p key={index}>{author}</p>))
-          : ""
-      }</div>
+    <div className="book-authors">{book.authors.map((author) => author + ", ")}</div>
   </div>);
 }
-
-Book.propTypes = {
-  book: PropTypes.object.isRequired,
-  setNewShelf: PropTypes.func.isRequired
-}
-
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Search from './components/Search/Search';
 
 ReactDOM.render(<Router>
-  <App/>
+  {/* <Route path="/" exact="exact" component={Index}/> */}
+  <div>
+    <App/>
+    <Route exact path="/search/" component={Search}/>
+  </div>
+  {/* <Route path="/users/" component={Users}/> */}
 </Router>, document.getElementById('root'))
